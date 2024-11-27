78b8ca8f570a49e1c0a1f694bc6d88a2be1517ab
juancardaw <juancarlosgym15@gmail.com>
2024-11-27 13:07:15 +0100

    Beta corregida

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,9 +5,9 @@ export const Footer = () => `
 
 <h2>Contacto</h2>
 <div>
-${Button("../../public/icons/x.jpeg", "X")}
-${Button("../../public/icons/github.jpeg", "GitHub")}
-${Button("../../public/icons/link.jpeg", "LinkedIn")}
-${Button("../../public/icons/telegram.jpeg", "Telegram")}
+${Button("../../icons/x.jpeg", "X")}
+${Button("../../icons/github.jpeg", "GitHub")}
+${Button("../../icons/link.jpeg", "LinkedIn")}
+${Button("../../icons/telegram.jpeg", "Telegram")}
 </div>
 `;
--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -9,7 +9,7 @@ export const Home = () => {
     <section class="home">
     <h3>Bienvenido a mi Portfolio</h3>
     <h1>Juan Carlos Piñas Muñoz</h1>
-    <img src="../../public/icons/fotoperfil.jpg">
+    <img src="../../icons/fotoperfil.jpg">
     <p>Soy un programador web con una sólida base en HTML, JAVA, JS, React y CSS. Me apasiona transformar ideas en realidad digital, creando soluciones web que combinan diseño atractivo con funcionalidad sólida. Mi enfoque se centra en desarrollar aplicaciones web responsivas, de alto rendimiento y fáciles de usar. Me adapto a todo tipo de trabajos implantando las mejores soluciones posibles.</p>
     <a href="mailto:juancarlosgym15@gmail.com">Si tienes un proyecto en mente, escribeme</a>
     </section>`;
